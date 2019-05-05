836f229d83fe0cb1c9d2cfcdeb77efd74d9ae102
sujan majumder <sujanmajumder211@gmail.com>
2019-05-05 11:32:32 +0600

    magniici popup gallery

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function ($){
+    "use stare";
+
+
+    $('.popup').magnificPopup({
+        type: 'image',
+        // other options
+
+        gallery: {
+            // options for gallery
+            enabled: true
+          },
+      });
+      
+
+
+})
